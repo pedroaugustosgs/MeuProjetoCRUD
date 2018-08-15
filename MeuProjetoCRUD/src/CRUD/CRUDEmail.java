61c8a16a6c2a76d7db223b4a5d671f8a95e86bdd
SENAI <SENAI@SENAI-03>
2018-08-15 16:35:00 -0300

    ghv

--- MeuProjetoCRUD/src/CRUD/CRUDEmail.java
+++ MeuProjetoCRUD/src/CRUD/CRUDEmail.java
@@ -4,7 +4,7 @@ import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
 public class CRUDEmail {
-	public void EmailVisuAulas(String prof,String materia,String conteudo) {
+	public void EmailVisuAulas(String prof,String materia,String conteudo,String emails) {
 		
 		String de = "projetojavasenai@gmail.com";
 		boolean ssl = true;
@@ -24,7 +24,7 @@ public class CRUDEmail {
 		email.setSmtpPort(Integer.parseInt(porta));
 		email.setAuthentication(usuario, senha);
 		
-		email.addTo("pedrovilelasgs@gmail.com");
+		email.addTo(emails);
 		email.setSubject("Aula Alterada");
 		email.setMsg(msg);
 		email.send();
