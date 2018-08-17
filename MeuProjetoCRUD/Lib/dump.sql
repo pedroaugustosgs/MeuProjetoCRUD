2dadaf2bff1cd66c59894b77d4c155556da9fa68
SENAI <SENAI@SENAI-01>
2018-08-17 14:46:57 -0300

    hhh

--- MeuProjetoCRUD/Lib/dump.sql
+++ MeuProjetoCRUD/Lib/dump.sql
@@ -428,6 +428,23 @@ UNLOCK TABLES;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: alunos
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
 -- Table structure for table `alunosconfirmados`
 --
@@ -439,7 +456,6 @@ CREATE TABLE `alunosconfirmados` (
   `idAlunosConfirmados` int(11) NOT NULL AUTO_INCREMENT,
   `idAluno` int(11) DEFAULT NULL,
   `idConfirmar` int(11) DEFAULT NULL,
-  `vagas` int(11) DEFAULT NULL,
   PRIMARY KEY (`idAlunosConfirmados`),
   UNIQUE KEY `idAlunosConfirmados_UNIQUE` (`idAlunosConfirmados`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -463,4 +479,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-17 14:10:44
+-- Dump completed on 2018-08-17 14:44:35
